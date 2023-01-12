18e2f246f5ef2cf5278d91e1243238eb099d8bcf
KDO <114388910+kdo2064@users.noreply.github.com>
2023-01-12 16:11:44 +0000

    Update index.js

--- other/index.js
+++ other/index.js
@@ -1,4 +1,4 @@
-var images = ["img/heart.png"]
+var images = ["/Valentine_Day/img/heart.png"]
 var imgState = 0;
 var imgTag = document.getElementById("rose");
 imgTag.addEventListener("click", function (event) {
@@ -7,7 +7,7 @@ event.target.src = images[imgState];
 document.getElementById("text").innerHTML = "Click on last teddy";
 });
 
-var I = ["img/I.png"]
+var I = ["/Valentine_Day/img/I.png"]
 var imgState = 0;
 var imgTag = document.getElementById("gift");
 imgTag.addEventListener("click", function (teddy) {
@@ -16,7 +16,7 @@ event.target.src = I[imgState];
 document.getElementById("on").innerHTML = "Click on Gift";
 });
 
-var u = ["img/you.png"]
+var u = ["/Valentine_Day/img/you.png"]
 var imgState = 0;
 var imgTag = document.getElementById("you");
 imgTag.addEventListener("click", function (teddy) {
